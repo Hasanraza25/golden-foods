9377d10a929f0b1afc8daedc70200859f86fe23c
Muhammad Saad <saadsaleem997@gmail.com>
2025-09-21 00:49:11 +0500

    categoriescard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -14,8 +14,8 @@ const HomePage = () => {
       <Hero />
       <AboutUs />
       <MissionVision />
-      <BannerPremium />
       <CategoryCards/>
+      <BannerPremium />
       <Footer />
     </>
   );
