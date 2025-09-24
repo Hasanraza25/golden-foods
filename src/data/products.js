0846dbf29419167c851445be9ffb335dd4e8cb27
Muhammad Saad <saadsaleem997@gmail.com>
2025-09-24 10:26:25 +0500

    productspage-without image

---
+++ src/data/products.js
@@ -0,0 +1,51 @@
+export const categories = [
+  "Golden Recipe Macaroni",
+  "Golden Recipie Spaghetti",
+  "Golden U-Shape Vermicelli",
+  "Golden Chaat Masala",
+  "Golden Classic Spaghetti",
+  "Golden Classic Macaroni",
+];
+
+export const productsData = {
+  "Golden Recipe Macaroni": [
+    { id: 1, name: "Golden Achari Macaroni 250g", price: 160 },
+    { id: 2, name: "Golden Tikka Macaroni 250g", price: 160 },
+    { id: 3, name: "Golden Creamy Alfredo Macaroni 250g", price: 160 },
+    { id: 4, name: "Golden Fajita Macaroni 250g", price: 160 },
+    { id: 5, name: "Golden 7 Spice Macaroni 250g", price: 160 },
+    { id: 6, name: "Golden Hot & Sour Macaroni 250g", price: 160 },
+    { id: 7, name: "Golden Green Tikka Macaroni 250g", price: 160 },
+    { id: 8, name: "Golden Chicken Karahi Macaroni 250g", price: 160 },
+    { id: 9, name: "Golden BBQ Macaroni 250g", price: 160 },
+    { id: 10, name: "Golden Achari Qeema Macaroni 250g", price: 160 },
+  ],
+
+  "Golden Classic Macaroni": [
+    { id: 22, name: "Golden Small Elbow Macaroni 400g", price: 210 },
+    { id: 23, name: "Golden Macaroni Box 400g", price: 210 },
+  ],
+
+  "Golden Recipie Spaghetti": [
+    { id: 11, name: "Golden Chicken Chowmein Spaghetti 250g", price: 160 },
+    { id: 12, name: "Golden Tikka Spaghetti 250g", price: 160 },
+    { id: 13, name: "Golden 7 Spice Spaghetti 250g", price: 160 },
+    { id: 14, name: "Golden Fajita Spaghetti 250g", price: 160 },
+    { id: 15, name: "Golden Achari Spaghetti 250g", price: 160 },
+  ],
+
+  "Golden Classic Spaghetti": [
+    { id: 19, name: "Golden Spaghetti 400g", price: 210 },
+    { id: 20, name: "Golden Spaghetti 450g", price: 230 },
+    { id: 21, name: "Golden Spaghetti 500g", price: 260 },
+  ],
+
+  "Golden Chaat Masala": [
+    { id: 16, name: "Golden Chat Masala 45g", price: 110 },
+  ],
+
+  "Golden U-Shape Vermicelli": [
+    { id: 17, name: "Golden U-Shape Vermicelli 100g", price: 50 },
+    { id: 18, name: "Golden U-Shape Vermicelli 130g", price: 65 },
+  ],
+};
